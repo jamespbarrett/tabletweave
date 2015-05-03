8f1e829852caafc90dbbf7ea4827553861e87d2b
James Weaver <james.barrett@bbc.co.uk>
2015-05-03 11:47:27 +0100

    Various bugfixes

--- draft.js
+++ draft.js
@@ -19,7 +19,18 @@ var defaultcell = {
     "direction":"left"
 };
 
+function drawOval(ctx, x, y, length, bredth, angle) {
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate(angle);
+    ctx.scale(bredth/length,1);
+    ctx.arc(0, 0, length/2, 2*Math.PI, false);
+    ctx.restore();
+}
+
 function redrawCanvas() {
+    Cookies.set("tablet-draft", { "main" : main_cells, "lower" : lower_cells}, { expires: 366 });
+
     var c = $("#draftcanvas");
     var ctx = c[0].getContext("2d");
 
@@ -37,34 +48,42 @@ function redrawCanvas() {
         for (x = 0; x < nCols; x++) {
             ctx.fillStyle = main_cells[y][x]["background_color"];
             ctx.fillRect(labelwidth + (cellborder + cellwidth)*x, (cellborder + cellheight)*y, cellwidth + cellborder, cellheight + cellborder);
-            ctx.strokeStyle = main_cells[y][x]["color"];
+            ctx.fillStyle = main_cells[y][x]["color"];
             ctx.beginPath();
             if (main_cells[y][x]["direction"] == "left") {
-                ctx.moveTo(labelwidth + cellborder + (cellborder + cellwidth)*x, cellborder + (cellborder + cellheight)*y);
-                ctx.lineTo(labelwidth + cellborder + (cellborder + cellwidth)*x + cellwidth, cellborder + (cellborder + cellheight)*y + cellheight);
+                drawOval(ctx,
+                         labelwidth + cellborder + (cellborder + cellwidth)*x + cellwidth/2,
+                         cellborder + (cellborder + cellheight)*y + cellheight/2,
+                         cellwidth, cellwidth/2, -Math.PI/4);
             } else {
-                ctx.moveTo(labelwidth + cellborder + (cellborder + cellwidth)*x + cellwidth, cellborder + (cellborder + cellheight)*y);
-                ctx.lineTo(labelwidth + cellborder + (cellborder + cellwidth)*x, cellborder + (cellborder + cellheight)*y + cellheight);
+                drawOval(ctx,
+                         labelwidth + cellborder + (cellborder + cellwidth)*x + cellwidth/2,
+                         cellborder + (cellborder + cellheight)*y + cellheight/2,
+                         cellwidth, cellwidth/2, Math.PI/4);
             }
             ctx.lineWidth = 4;
-            ctx.stroke();
+            ctx.fill();
         }
     }
     for (y = 0; y < nRowsLow; y++) {
         for (x = 0; x < nCols; x++) {
-            ctx.fillStyle = lower_cells[y][x]["background_color"];
+            ctx.fillStyle = "#ffffff";//lower_cells[y][x]["background_color"];
             ctx.fillRect(labelwidth + (cellborder + cellwidth)*x, (cellborder + cellheight)*nRowsMain + intertablegap + (cellborder + cellheight)*y, cellwidth + cellborder, cellheight + cellborder);
-            ctx.strokeStyle = lower_cells[y][x]["color"];
+            ctx.fillStyle = lower_cells[y][x]["color"];
             ctx.beginPath();
             if (lower_cells[y][x]["direction"] == "left") {
-                ctx.moveTo(labelwidth + cellborder + (cellborder + cellwidth)*x, (cellborder + cellheight)*nRowsMain + intertablegap + cellborder + (cellborder + cellheight)*y);
-                ctx.lineTo(labelwidth + cellborder + (cellborder + cellwidth)*x + cellwidth, (cellborder + cellheight)*nRowsMain + intertablegap + cellborder + (cellborder + cellheight)*y + cellheight);
+                drawOval(ctx,
+                         labelwidth + cellborder + (cellborder + cellwidth)*x + cellwidth/2,
+                         (cellborder + cellheight)*nRowsMain + intertablegap + cellborder + (cellborder + cellheight)*y + cellheight/2,
+                         cellwidth, cellwidth/2, -Math.PI/4);
             } else {
-                ctx.moveTo(labelwidth + cellborder + (cellborder + cellwidth)*x + cellwidth, (cellborder + cellheight)*nRowsMain + intertablegap + cellborder + (cellborder + cellheight)*y);
-                ctx.lineTo(labelwidth + cellborder + (cellborder + cellwidth)*x, (cellborder + cellheight)*nRowsMain + intertablegap + cellborder + (cellborder + cellheight)*y + cellheight);
+                drawOval(ctx,
+                         labelwidth + cellborder + (cellborder + cellwidth)*x + cellwidth/2,
+                         (cellborder + cellheight)*nRowsMain + intertablegap + cellborder + (cellborder + cellheight)*y + cellheight/2,
+                         cellwidth, cellwidth/2, Math.PI/4);
             }
             ctx.lineWidth = 4;
-            ctx.stroke();
+            ctx.fill();
         }
     }
     ctx.strokeStyle = "#000000";
@@ -95,8 +114,6 @@ function redrawCanvas() {
     for (y = 0; y < nRowsLow; y++) {
         ctx.fillText(labels[y], 2, (cellborder + cellheight)*nRowsMain + intertablegap + (cellborder + cellheight)*y + (cellheight + 15)/2);
     }
-
-    Cookies.set("tablet-draft", { "main" : main_cells, "lower" : lower_cells });
 }
 
 function updateSizes(m,l,c) {
@@ -114,14 +131,14 @@ function updateSizes(m,l,c) {
     $("#colcontrols .readout").val(c);
 
     if (main_cells.length > m) {
-        main_cells = main_cells.slice(0,m);
+        main_cells = main_cells.slice(main_cells.length - m, main_cells.length);
     } else if (main_cells.length < m) {
-        for (y = main_cells.length; y < m; y++) {
+        for (y = 0; y < m - main_cells.length; y++) {
             var row = [];
             for (x = 0; x < c; x++) {
                 row[x] = JSON.parse(JSON.stringify(defaultcell));
             }
-            main_cells[y] = row;
+            main_cells.unshift(row);
         }
     }
 
@@ -164,22 +181,37 @@ function cellClick(g,x,y) {
     var cell;
     if (g == 0) {
         cell = main_cells[y][x];
+
+        if (fgcol != "none") {
+            cell["color"] = fgcol;
+        }
+        if (bgcol != "none") {
+            cell["background_color"] = bgcol;
+        }
+
+        if (fgcol == "none" && bgcol == "none") {
+            if (cell["direction"] == "left") {
+                cell["direction"] = "right";
+            } else {
+                cell["direction"] = "left";
+            }
+        }
     } else {
         cell = lower_cells[y][x];
-    }
 
-
-    if (fgcol != "none") {
-        cell["color"] = fgcol;
-    }
-    if (bgcol != "none") {
-        cell["background_color"] = bgcol;
-    }
-
-    if (cell["direction"] == "left") {
-        cell["direction"] = "right";
-    } else {
-        cell["direction"] = "left";
+        if (fgcol != "none") {
+            cell["color"] = fgcol;
+        } else {
+            if (cell["direction"] == "left") {
+                for (Y = 0; Y < lower_cells.length; Y++) {
+                    lower_cells[Y][x]["direction"] = "right";
+                }
+            } else {
+                for (Y = 0; Y < lower_cells.length; Y++) {
+                    lower_cells[Y][x]["direction"] = "left";
+                }
+            }
+        }
     }
 
     redrawCanvas();
@@ -210,16 +242,8 @@ function canvasClick(e) {
 function setForegroundColor(id) {
     if (id == "EMPTYBOX") {
         fgcol = "none";
-    } else if (id == "WHITEBOX") {
-        fgcol = "#ffffff";
-    } else if (id == "BLACKBOX") {
-        fgcol = "#000000";
-    } else if (id == "REDBOX") {
-        fgcol = "#ff0000";
-    } else if (id == "GREENBOX") {
-        fgcol = "#00ff00";
-    } else if (id == "BLUEBOX") {
-        fgcol = "#0000ff";
+    } else {
+        fgcol = $("#palete #fg #" + id).css('backgroundColor');
     }
 
     $("#palete #fg .colorbox").each(function() {
@@ -244,6 +268,7 @@ function setBackgroundColor(id) {
 }
 
 function exportImage(mimetype) {
+    Cookies.set("tablet-draft", { "main" : main_cells, "lower" : lower_cells}, { expires: 366 });
     var c = $("#draftcanvas")[0];
     var image = c.toDataURL(mimetype);
     window.open(image);
@@ -274,9 +299,9 @@ $(function() {
         updateSizes(1,1,1);
     });
 
-    $("#mainrowcontrols .readout").val(main_cells[0].length);
-    $("#lowrowcontrols .readout").val(lower_cells[0].length);
-    $("#colcontrols .readout").val(main_cells.length);
+    $("#mainrowcontrols .readout").val(main_cells.length);
+    $("#lowrowcontrols .readout").val(lower_cells.length);
+    $("#colcontrols .readout").val(main_cells[0].length);
 
     $("#mainrowcontrols .readout").change(function() { updateSizes(parseInt($("#mainrowcontrols .readout").val()),
                                                                    parseInt($("#lowrowcontrols .readout").val()),
