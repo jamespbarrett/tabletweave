b8fc155db99b09e5e8f70fec6cf02a5c7c6a80fe
James Weaver <james.barrett@bbc.co.uk>
2019-12-29 14:22:09 +0000

    Under development

--- fmt/tdd2svg.js
+++ fmt/tdd2svg.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
+/* Set up a fake DOM to make it seem that we are loaded in a browser */
 var jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-
 const { window } = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`).window;
 
 console.log(window.document.querySelector("p").textContent);
+
